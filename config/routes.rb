9c19e30082383200aed1b7ed3abf1466466275f2
Robert <robt15@hotmail.com>
2020-02-23 23:30:59 -0500

    sunday

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/authenticate', to: 'authentication#authenticate'
   post 'users/new', to: "users#create"
   get '/user', to: 'users#show'
   patch '/user', to: 'users#update'
